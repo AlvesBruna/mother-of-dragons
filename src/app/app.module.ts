c56e88a834b68cd34bc6fc3068b67fe90c0af166
Bruna Alves <brunalves@MacBook-Pro-de-dbserver-3.local>
2019-06-15 22:30:05 -0300

    Add login component and route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { environment } from '../environments/environments';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment),
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required, 
+      Validators.email
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+    ]),
+  });
+
+  constructor(public auth: AuthService, public router: Router) { }
+
+  error = error => ({
+    required: 'You missed a spot! Don\'t forget to add your email and password.',
+    email: 'Hmm... that doesn\'t look like an email address.'
+  }[error])
+
+  setFormError = (error) =>{
+    this.loginForm.setErrors({
+      message: this.error(error)
+    })
+  }
+
+  ngOnInit = () => {
+    this.loginForm.reset();
+  }
+
+  isLoginIvalid = () => {
+    const emailError = this.loginForm.get('email').errors;
+    const passwordError = this.loginForm.get('password').errors;
+
+    if (!emailError && !passwordError) return false;
+
+    const errors = Object.keys({...emailError, ...passwordError});
+    errors.forEach(this.setFormError)
+
+    return true;
+  }
+
+  login = () => {
+    if(this.isLoginIvalid()) return;
+
+    const user = {
+      email: this.loginForm.get('email').value,
+      password: this.loginForm.get('password').value,
+    }
+
+    this.auth.login(user)
+      .then(() => {
+        this.router.navigate(['/'])
+      })
+      .catch(err => this.loginForm.setErrors({message: err.message}));
+  }
+
+}
